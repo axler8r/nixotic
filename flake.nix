8a49eabe5faee8d3c6447a0aa6c8241450525e65
AxlER8R <axl@axler8r.io>
2026-01-07 20:45:33 +1300

    feat: generate flake lock file

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Nixotic - NixOS configuration for axl";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      nixosConfigurations.nix000 = nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./hosts/nix000/configuration.nix
+
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.axl = import ./home/default.nix;
+          }
+        ];
+      };
+    };
+}
