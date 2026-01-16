79e8b5f76f4618b4223e293ebaccef01cfc9a8fe
AxlER8R <axl@axler8r.io>
2026-01-16 17:46:54 +1300

    wip: speed up NixOS build

--- hosts/demonstr8r/configuration.nix
+++ hosts/demonstr8r/configuration.nix
@@ -70,7 +70,23 @@
   # User required system features
   ####################################################################
   nixpkgs.config.allowUnfree = true;
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings = {
+    experimental-features = [ "nix-command" "flakes" ];
+
+    # Build performance
+    max-jobs = "auto";
+    cores = 0;
+
+    # Additional binary caches
+    substituters = [
+      "https://cache.nixos.org"
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
   nix.gc = {
     automatic = true;
     dates = "weekly";
