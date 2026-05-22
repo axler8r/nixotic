e34888ef90bef95dfc6b1c17dd76c64903842528
AxlER8R <axler8r@pm.me>
2026-05-22 20:02:33 +1200

    feat(illumin8r): add cli tools

--- home/wsl.nix
+++ home/wsl.nix
@@ -3,9 +3,17 @@
 {
   imports = [
     ./base.nix
+    ./claude.nix
+    ./eza.nix
+    ./fd.nix
+    ./gh.nix
+    ./jq.nix
+    ./ripgrep.nix
   ];
 
   home.packages = with pkgs; [
+    claude-code
+    curl
     github-copilot-cli
   ];
 
