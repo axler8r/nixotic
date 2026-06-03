35e0c852aa9df9a4d5abdf68ed8a3380bf9a265a
AxlER8R <axler8r@pm.me>
2026-06-03 17:03:20 +1200

    feat!(shell): refine development environment management
    add:
    - Remove-DevEnvironment: removes flake.nix, .envrc, .direnv/ with optional --gc
    - ZSH completions for New-DevEnvironment and Remove-DevEnvironment
    - home.packages runCommand derivation to ship completions onto fpath
    
    modify:
    - trim language templates to python, elixir, dotnet
    - drop --ml and --tool flags
    - minimal package sets; let language tooling manage the rest
    - replace shellHook with mkShell attributes for env vars
    - single flake template, no ML/non-ML branching
    
    feat! because the removal of --ml, --tool, and four language templates breaks the existing interface.

--- home/zsh.nix
+++ home/zsh.nix
@@ -181,4 +181,12 @@ in
       ${pkgs.uv}/bin/uv generate-shell-completion zsh > $out
     ''
   );
+
+  home.packages = [
+    (pkgs.runCommand "zsh-completions-nixotic" { } ''
+      mkdir -p $out/share/zsh/site-functions
+      cp ${../files/zsh/completions/_New-DevEnvironment} $out/share/zsh/site-functions/_New-DevEnvironment
+      cp ${../files/zsh/completions/_Remove-DevEnvironment} $out/share/zsh/site-functions/_Remove-DevEnvironment
+    '')
+  ];
 }
